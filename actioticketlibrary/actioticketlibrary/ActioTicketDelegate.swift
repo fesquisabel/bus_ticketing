b3b1b9825b07cf1ea3bcb40d0f0e52dfe902aa52
Fermín Esquisábel <fermin.esquisabel@geoactio.com>
2024-01-19 19:47:10 +0100

    Prepare workspace

---
+++ actioticketlibrary/actioticketlibrary/ActioTicketDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  ActioTicketDelegate.swift
+//  actioticketlibrary
+//
+//  Created by Fermin Esquisabel Garcia on 19/1/24.
+//
+
+import Foundation
+
+class ActioTicketDelegate {
+    
+    init() {
+        
+    }
+    
+}
---
+++ actioticketlibrary/actioticketlibrary/actioticketlibrary.h
@@ -0,0 +1,18 @@
+//
+//  actioticketlibrary.h
+//  actioticketlibrary
+//
+//  Created by Fermin Esquisabel Garcia on 19/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for actioticketlibrary.
+FOUNDATION_EXPORT double actioticketlibraryVersionNumber;
+
+//! Project version string for actioticketlibrary.
+FOUNDATION_EXPORT const unsigned char actioticketlibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <actioticketlibrary/PublicHeader.h>
+
+
